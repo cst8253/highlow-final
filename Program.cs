e566284af37abaddb5715150ce806ac609a70db1
Michael Eisenbraun <eisenbm@algonquincollege.com>
2023-05-16 10:32:50 -0400

    Creates final version

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace HighLow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // create Random object
+            Random random = new Random();
+
+            // random int from range, max is excusive
+            int secret = random.Next(1, 11);
+            bool isActive = true;
+            bool isValid;
+            int guess;
+            int remainingGuesses = 3;
+
+            do
+            {
+                Console.WriteLine("Guess a number between 1 and 10");
+                isValid = int.TryParse(Console.ReadLine(), out guess);
+                
+                if (isValid)
+                {
+                    remainingGuesses--;
+
+                    if (guess == secret)
+                    {
+                        Console.WriteLine("You guessed the number!");
+                        isActive = false;
+                    }
+                    else
+                    {
+                        if (remainingGuesses == 0)
+                        {
+                            Console.WriteLine("Sorry, you are out of guesses. The number was " + secret);
+                            isActive = false;
+                        }
+                        else if (guess < secret)
+                        {
+                            Console.WriteLine($"Sorry, you guessed too low. You have {remainingGuesses} guesses remaining.");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Sorry, you guess too high. You have {remainingGuesses} guesses remaining.");
+                        }
+                    }
+                } 
+                else
+                {
+                    Console.WriteLine("Not a valid guess. Try again.");
+                }
+
+                if (!isActive)
+                {
+                    Console.WriteLine("Play Again? (Y)");
+                    string response = Console.ReadLine();
+
+                    if (response.Equals("Y", StringComparison.OrdinalIgnoreCase))
+                    {
+                        secret = random.Next(1, 11);
+                        guess = 0;
+                        remainingGuesses = 3;
+                        isActive = true;
+                    }
+                }
+            } while (isActive);
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# High Low
+
+## Objective
+Create a console application for a game of High Low. A random number is chosen between 1 and 10. The player has three guesses to get the right number. After each wrong guess, the player is informed if they are high or low. 
